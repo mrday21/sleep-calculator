599e574863a939a79c838e2fe0a79c81c161df3c
Brian Day <daycoderindustries@gmail.com>
2019-02-09 23:21:10 +0000

    Added About page and background image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,14 @@ class PagesController < ApplicationController
  # Get for / which is home page
   def home
   end
+  
   def index
   end
+  
   def calculator
   end
+  
+  def about
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'calculator', to: 'pages#calculator'
+  get 'about', to: 'pages#about'
 end
